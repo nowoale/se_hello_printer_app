f81a38c4da0df2bde466ce0537975f1ce7c2a892
nowoale <nowoale@users.noreply.github.com>
2021-04-25 14:50:32 +0200

    zmiany niedziela 1

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,17 +3,34 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Alek"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
     output = request.args.get('output')
+    name = request.args.get('name')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
+    if not name:
+        name = moje_imie
+    return get_formatted(msg, name,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
+
+
+@app.route('/name')
+def name_changer():
+    name = request.args.get('name')
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+    if name is None:
+        return get_formatted(msg, moje_imie, output.lower())
+    else:
+        return get_formatted(msg, name, output.lower())
